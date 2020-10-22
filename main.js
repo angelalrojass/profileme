43638a592e618593ac13e7e034163a0a3c566cd5
angel <angelalrojas_12@hotmail.com>
2020-10-21 21:37:33 -0500

    fisrtaa

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".nav-menu").classList.toggle("show");
-  });
+  document.querySelector(".nav-menu").classList.toggle("show");
+});
 
-
-
-  
-  ScrollReveal().reveal('.showcase');
-  ScrollReveal().reveal('.news-cards', { delay:500 });
-  ScrollReveal().reveal('.news-cards-two', { delay: 1000 });
-  
+ScrollReveal().reveal('.showcase',{reset: true});
+ScrollReveal().reveal('.news-cards', { delay:500, duration: 500, reset: true });
+ScrollReveal().reveal('.news-cards-two', { delay: 1000, duration: 1000, reset: true });
+ScrollReveal().reveal('.showhability', { delay: 1500, duration: 1500, reset: true });
